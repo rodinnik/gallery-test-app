021fcddf59e7513ca1bb83806455ba81114e7406
rodinnik <lkjhgbvlm@mail.ru>
2022-09-15 16:11:59 +0400

    edit

--- src/components/Range.jsx
+++ src/components/Range.jsx
@@ -30,18 +30,29 @@ class Range extends React.Component {
         >
           <span className="select-current">Created</span>
           <div className="select-icon">
-            <svg
-              className="select-pointer"
-              width="10"
-              height="6"
-              viewBox="0 0 10 6"
-              fill="#555555"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M9.67861 1.8337L5.77064 5.68539C5.34503 6.10487 4.65497 6.10487 4.22936 5.68539L0.321394 1.8337C-0.365172 1.15702 0.121082 -8.3659e-08 1.09203 0L8.90797 6.73452e-07C9.87892 7.57113e-07 10.3652 1.15702 9.67861 1.8337Z"
-              />
-            </svg>
+            {this.state.isActive ? (
+              <svg
+                className="select-pointer"
+                width="10"
+                height="6"
+                viewBox="0 0 10 6"
+                fill="#555555"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M0.321395 4.1663L4.22936 0.314613C4.65497 -0.104871 5.34503 -0.104871 5.77064 0.314613L9.67861 4.1663C10.3652 4.84298 9.87892 6 8.90797 6L1.09203 6C0.121082 6 -0.365172 4.84298 0.321395 4.1663Z" />
+              </svg>
+            ) : (
+              <svg
+                className="select-pointer"
+                width="10"
+                height="6"
+                viewBox="0 0 10 6"
+                fill="#555555"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M9.67861 1.8337L5.77064 5.68539C5.34503 6.10487 4.65497 6.10487 4.22936 5.68539L0.321394 1.8337C-0.365172 1.15702 0.121082 -8.3659e-08 1.09203 0L8.90797 6.73452e-07C9.87892 7.57113e-07 10.3652 1.15702 9.67861 1.8337Z" />
+              </svg>
+            )}
           </div>
         </div>
 
@@ -59,7 +70,7 @@ class Range extends React.Component {
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
             >
-              <line x1="0.5" y1="0.5" x2="12.5" y2="0.5" stroke="white" />
+              <line x1="0.5" y1="0.5" x2="12.5" y2="0.5" />
             </svg>
             <input
               type="text"
